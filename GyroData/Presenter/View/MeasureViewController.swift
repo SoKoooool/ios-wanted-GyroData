7bb989c66e7872ed6edf36a26ac7f264adaf7df7
Tak <oit5538@gmail.com>
2022-12-28 17:18:28 +0900

    feat: 화면전환을 위한 MeasureViewController 임시생성

---
+++ GyroData/Presenter/View/MeasureViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureViewController.swift
+//  GyroData
+//
+//  Created by Tak on 2022/12/28.
+//
+
+import UIKit
+
+class MeasureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+}
